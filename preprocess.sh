91d14c7a58fe9898c44edec5992ee8a36150b6b8
nipeng <nipeng@iqianjin.com>
2019-12-04 11:49:22 +0800

    add sh

---
+++ preprocess.sh
@@ -0,0 +1,3 @@
+python preprocess.py --base_dir /data/liuchengwei/tacotron-keithito/datasets \
+  --dataset ljspeech \
+  --num_workers 100
---
+++ train.sh
@@ -0,0 +1,2 @@
+CUDA_VISIBLE_DEVICES=3 python train.py --base_dir /data/liuchengwei/tacotron-keithito/ \
+  --input datasets/training/train.txt
